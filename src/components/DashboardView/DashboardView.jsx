0ba75228d3fbec3f38a27f50d94166f8e3a70df4
Davi Piassi Barros dos Santos <davibarros-ieg@instituto.ad>
2024-10-16 13:10:19 -0300

    setting up dynamic dashs

--- src/components/DashboardView/DashboardView.jsx
+++ src/components/DashboardView/DashboardView.jsx
@@ -2,6 +2,15 @@ import styles from './DashboardView.module.css'
 
 export default function DashboardView({url}) {
   return (
-    <section className={styles.container}>{url}</section>
+    <section className={styles.container}>
+      <iframe 
+        title="inter" 
+        width="100%" 
+        height="100%" 
+        src={url} 
+        frameborder="0" 
+        allowFullScreen="true">
+        </iframe>
+    </section>
   )
 }
