57e3526f76e72d611de78a13969daac82d1dacac
Davi Piassi Barros dos Santos <davibarros-ieg@instituto.ad>
2024-11-05 17:18:57 -0300

    removing comments

--- src/shared/helpers/date-formatter.js
+++ src/shared/helpers/date-formatter.js
@@ -1,7 +1,7 @@
 export function formatDate(date) {
     const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Meses começam em 0
-    const year = String(date.getFullYear()).slice(-2); // Pega os dois últimos dígitos do ano
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = String(date.getFullYear()).slice(-2);
     const hours = String(date.getHours()).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
 
